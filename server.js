58b6fecd5f360bcf663ef00f363c1200f9e040ec
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2017-05-15 10:58:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express    =    require('express');
 var app        =    express();
+
+app.get('/',function(req,res){
+    res.send('Testing, testing, one, two, three...');
+});
+
 var server     =    app.listen(3000,function(){
     console.log("Server is started on port 3000");
 });
